abcfc0877c198e01e9aa8b81edd76c8884683127
Renato Lui Geh <renatogeh@gmail.com>
2014-04-03 22:39:41 -0300

    Minor changes. They're not allowed to drink yet.

--- class.h
+++ class.h
@@ -3,26 +3,26 @@
 
 /* Definition of a class. */
 #define class(name) typedef struct __struct_##name ##name; \
-					##name* ##name::init(); \
-					void ##name::##name(##name*); \
-					void ##name::destroy(##name*); \
-					void ##name::~##name(##name*); \
-					struct __struct_##name
+                    ##name* ##name::init(); \
+                    void ##name::##name(##name*); \
+                    void ##name::destroy(##name*); \
+                    void ##name::~##name(##name*); \
+                    struct __struct_##name
 
 class(Object) {
-	int id;
+    int id;
 };
 
 /* Definition of a constructor of a class. */
 
-#define constructor(name) 	##name* ##name::init() { \
-								##name* selfie = (##name*)malloc(sizeof(##name)); \
-								##name::##name(selfie); \
-								return selfie; \
-							} void ##name::##name(##name* this)
+#define constructor(name)   ##name* ##name::init() { \
+                                ##name* selfie = (##name*)malloc(sizeof(##name)); \
+                                ##name::##name(selfie); \
+                                return selfie; \
+                            } void ##name::##name(##name* this)
 
 constructor(Object) {
-	this->id = 0;
+    this->id = 0;
 } 
 
 /* Definition of the 'new' keyword. */
@@ -30,10 +30,10 @@ constructor(Object) {
 
 
 /* Definition of a destructor of a class. */
-#define destructor(name) 	##name* ##name::destroy(##name* selfie) { \
-								##name::~##name(selfie); \
-								free(selfie); \
-							} void ##name::~##name(##name* this)
+#define destructor(name)    ##name* ##name::destroy(##name* selfie) { \
+                                ##name::~##name(selfie); \
+                                free(selfie); \
+                            } void ##name::~##name(##name* this)
 
 destructor(Object) {
 
@@ -42,13 +42,13 @@ destructor(Object) {
 /* Definition of the 'delete' keyword. */
 #define delete(name, inst) ##name::destroy(inst);
 
-/** Thus a complete example on this pseudo-OOP system would be:
+/*  Thus a complete example on this pseudo-OOP system would be:
  *
  *  class(YTwist) {
- *		char* error = "Unexpected ending.";
- *		List elements;
- *		int current;
- *		// A function do_the_twist:R->R that twists the Y-axis.
+ *      char* error = "Unexpected ending.";
+ *      List elements;
+ *      int current;
+ *      // A function do_the_twist:R->R that twists the Y-axis.
  *      double (*do_the_twist)(YTwist*, double); 
  *  }
  *
@@ -57,20 +57,20 @@ destructor(Object) {
  *  Now on to the actual body implementation of the class YTwist.
  *  
  *  double do_the_twist_impl(YTwist* this, double x) {
- *		double y = doStuff(x);
- *		this->elements->add(y);
- *		++current;
- *	}
+ *      double y = doStuff(x);
+ *      this->elements->add(y);
+ *      ++current;
+ *  }
  *
  *  constructor(YTwist) {
- *		this->elements = new(List);
- *		this->current = 0;
- *		this->do_the_twist = do_the_twist_impl;
- *	}
+ *      this->elements = new(List);
+ *      this->current = 0;
+ *      this->do_the_twist = do_the_twist_impl;
+ *  }
  *
  *  destructor(YTwist) {
- *		delete(List, this->elements);
- *	}
+ *      delete(List, this->elements);
+ *  }
  *
  *  In this system both constructor and destructor MUST be explicitly
  *  implemented.
@@ -78,14 +78,14 @@ destructor(Object) {
  *  function implementations. Or at least their prototypes.
  *  
  *  int main(int argc, char* args[]) {
- *		YTwist* twister = new(YTwist);
- *		double value;
- *		value = twist_and_shout(twister);
- *		if(twister->current == 0) puts(twister->error);
- *		else twister->do_the_twist(twister, value);
- *		delete(YTwist, twister)
- *		return 0;
- *	}
+ *      YTwist* twister = new(YTwist);
+ *      double value;
+ *      value = twist_and_shout(twister);
+ *      if(twister->current == 0) puts(twister->error);
+ *      else twister->do_the_twist(twister, value);
+ *      delete(YTwist, twister)
+ *      return 0;
+ *  }
 */
 
 #endif
